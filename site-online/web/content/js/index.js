3a5f0a335b04bad432a24971729a2148ab05d062
Kaiesh Vohra <kaiesh@kaiesh.com>
2023-06-03 14:56:10 +0800

    Add demo animation

--- site-online/web/content/js/index.js
+++ site-online/web/content/js/index.js
@@ -85,5 +85,52 @@
     ).then(function(output){
       document.getElementById("builder_out").value = output;
     });
-  })
+  });
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function demoscript(caller){
+    caller.disabled = true;
+    let demoarr = [
+      {"elem": "#abi-network", "scale": 1.25, "delay": 1},
+      {"elem": "#infura_id", "scale": 1.25, "delay": 1, "text":"abcdefg"},
+      {"elem": "#abi1-name", "scale": 1.25, "delay": 1, "text":"MyContract"},
+      {"elem": "#abi1-ct", "scale": 1.25, "delay": 1, "text":"0x1234567890"},
+      {"elem": "#abi1-url", "scale": 1.25, "delay": 1, "text":'[{\n\t"inputs": [],\n\t"name": "numberOfMints",\n\t"outputs": [\n\t\t{\n\t\t"internalType": "uint256",\n\t\t"name": "",\n\t\t"type": "uint256"\n\t\t}],\n\t"stateMutability": "view",\n\t"type": "function"\n}]'},
+      {"elem": "#build", "scale": 1.25, "delay": 1, "event":"click"},
+      {"elem": "#builder_out", "scale": 1.25, "delay": 1},
+    ];
+    for (let i=0; i < demoarr.length; i++){
+      let tgt = document.querySelector(demoarr[i]["elem"]);
+      tgt.scrollIntoView({behavior: "smooth", block: "center"});
+      tgt.style.transition = "1s ease";
+      tgt.style.transform = "scale("+demoarr[i]["scale"]+")";
+      await sleep(1000);
+      if (demoarr[i]["text"] && demoarr[i]["text"].length > 0){
+        let perCharDuration = Math.round(2000 / demoarr[i]["text"].length);
+        perCharDuration = Math.max(perCharDuration, 1);
+        for (let j=0; j <= demoarr[i]["text"].length; j++){
+          tgt.value = demoarr[i]["text"].substring(0, j);
+          if (tgt.tagName.toUpperCase() == "TEXTAREA"){
+            tgt.scrollTop = tgt.scrollHeight;
+          }
+          await sleep(perCharDuration);
+        }
+      }
+      if (demoarr[i]["event"]){
+        setTimeout(function(){
+          tgt[demoarr[i]["event"]]();
+        },10);
+      }
+      tgt.style.transform = "scale(1)";
+      await sleep(demoarr[i]["delay"]*1000);
+      tgt.style.transition = "";
+    }
+    caller.disabled = false;
+  }
+  document.getElementById("seedemo").addEventListener("click", function(){
+    demoscript(this);
+  });
 })(window)
